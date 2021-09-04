6aa788fa19d8b543b94cb620e3c852e6f61e1373
Scott Dutton <scott@exussum.co.uk>
2021-09-04 22:05:11 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#Weatherspoons Locations
+
+## What is this
+
+A quick script to get all weatherpsoons pub information from their search endpoint
+
+## Why would I want this ?
+Showing locations on a map for example to find the closest one to another location
+
+
+# Applogies
+
+Lewis, if you are reading this. I'm sorry!
---
+++ get.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl 'https://www.jdwetherspoon.com/api/advancedsearch' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0' -H 'Accept: application/json, text/plain, */*'  --compressed -H 'Content-Type: application/json;charset=utf-8'  --data-raw '{"region":null,"paging":{"UsePagination":false},"facilities":[],"searchType":0}' > spoons.json
+
+jq -r '.regions [].subRegions [].items [] | del(.facilities) | keys |  @csv'  spoons.json | uniq >  spoons.csv
+
+jq -r '.regions [].subRegions [].items| del(.[].facilities) | (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $rows[] | @csv' spoons.json >> spoons.csv
